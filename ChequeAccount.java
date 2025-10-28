6796b22f2d7ffff0ed034668c89fe7e5643a4a74
Tlhalefomogote <cse24-057@thuto.bac.ac.bw>
2025-10-28 08:06:24 +0200

    Remove ChequeAccount class implementation

--- ChequeAccount.java
+++ ChequeAccount.java
@@ -1,14 +1,1 @@
-package model;
 
-public class ChequeAccount extends model.Account implements model.Depositable, model.Withdrawable {
-    private model.Company employer;
-
-    public ChequeAccount(String accNo, model.Customer c, String branch, double opening, model.Company employer) {
-        super(accNo, c, branch, opening);
-        if (employer == null)
-            throw new IllegalArgumentException("Employer required for ChequeAccount");
-        this.employer = employer;
-    }
-
-    public model.Company getEmployer() { return employer; }
-}
